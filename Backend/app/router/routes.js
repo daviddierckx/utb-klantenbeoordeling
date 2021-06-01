a01439aef19d28996e3cae5287f333e2f147ae57
Pistrie <6153719-Pistrie@users.noreply.gitlab.com>
2021-06-01 17:04:58 +0200

    Added normal user routes

--- Backend/app/router/routes.js
+++ Backend/app/router/routes.js
@@ -20,8 +20,8 @@ router.use(function timeLog(req, res, next) {
     const token = (req.header("authorization") ?? "").replace('Bearer ', '');
     console.log("token: ", token);
 
-    jwt.verify(token, config.auth.secret, {}, function (err, decoded) {
-        if (err) return res.status(401).send({"success": false, "error": "Unauthorized"});
+    jwt.verify(token, config.auth.secret, {}, function(err, decoded) {
+        if (err) return res.status(401).send({ "success": false, "error": "Unauthorized" });
         req.user_email = decoded.user_email;
         req.user_id = decoded.user_id;
         logger.log("User authorization success:", JSON.stringify(decoded));
@@ -33,7 +33,18 @@ router.post('/register', user_controller.register);
 router.post('/login', user_controller.login);
 
 // Add router here
-router.get('/:formId/addAnswer', forms_controller.enter_form_answer);
+// must haves
+router.get('/feedback/:formId/addAnswer', forms_controller.enter_form_answer);
+router.get('/feedback/:formType/:pageNr', method);
+router.get('/feedback', method);
+router.get('/complaints/overview', method);
+// should haves
+router.get('/api/feedback/overview');
+// could haves
+router.get('/complaints/overview', method);
+router.post('/complaints', method);
+router.put('/complaints/:complaintNr', method);
+router.get('/complaints/:complaintNr', method);
 
 
 module.exports = router
