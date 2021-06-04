c753ba9cbe860dba0167456d1ab63e205f2d9853
onno204 <15333546+onno204@users.noreply.github.com>
2021-06-04 10:59:28 +0200

    Added form 500 route

--- Backend/app/router/controllers/formsController.js
+++ Backend/app/router/controllers/formsController.js
@@ -5,3 +5,8 @@ exports.enter_form_answer = function (req, res) {
     logger.log("Received request to add a form answers");
     return res.status(500).send({"success": false, "error": "not implemented yet"});
 };
+
+exports.add_new_form = function (req, res) {
+    logger.log("Received request to create a new form");
+    return res.status(500).send({"success": false, "error": "not implemented yet"});
+};
