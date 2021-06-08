844486b3803f5c80a3fc741e2dfe5de822cdfa2f
Akalster <72736983+Akalster@users.noreply.github.com>
2021-06-08 11:35:05 +0200

    Route fix

--- Backend/app/router/admin.js
+++ Backend/app/router/admin.js
@@ -21,22 +21,22 @@ router.get("", (req, res) => {
 // - sylvester
 
 // must haves
-router.get('/admin/console', forms_controller.placeholder);
-router.post('/login', forms_controller.placeholder);
-router.get('/admin/feedback/overview', forms_controller.placeholder);
-router.delete('/admin/feedback/:feedbackId', forms_controller.placeholder);
-router.put('/admin/feedback/:feedbackId', forms_controller.placeholder);
-router.get('/admin/users/overview', forms_controller.placeholder);
-router.post('/admin/users', forms_controller.placeholder);
-router.delete('/admin/users/:userId', forms_controller.placeholder);
-router.put('/admin/users/:userId', forms_controller.placeholder);
+router.get('/console', forms_controller.placeholder); //staat nu op /admin maar is af
+router.post('/login', forms_controller.placeholder); //done
+router.get('/feedback/overview', forms_controller.placeholder);
+router.delete('/feedback/:feedbackId', forms_controller.placeholder);
+router.put('/feedback/:feedbackId', forms_controller.placeholder);
+router.get('/users/overview', forms_controller.placeholder);
+router.post('/users', forms_controller.placeholder);
+router.delete('/users/:userId', forms_controller.placeholder);
+router.put('/users/:userId', forms_controller.placeholder);
 // should haves
-router.post('/admin/feedback/:formId', forms_controller.placeholder);
-router.delete('/admin/feedback/:formId', forms_controller.placeholder);
-router.put('/admin/feedback/:formId', forms_controller.placeholder);
+router.post('/feedback/:formId', forms_controller.placeholder);
+router.delete('/feedback/:formId', forms_controller.placeholder);
+router.put('/feedback/:formId', forms_controller.placeholder);
 router.get('/feedback/overview/:date', forms_controller.placeholder);
 router.get('/feedback/overview/:rating', forms_controller.placeholder);
-router.get('/admin/admin/feedback/:formId', forms_controller.placeholder);
+router.get('/feedback/:formId', forms_controller.placeholder);
 // could haves
 router.get('/feedback/overview/:orderNr', forms_controller.placeholder);
 router.delete('/complaints/:complaintNr', forms_controller.placeholder);
