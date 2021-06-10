35aef961ff2c79b5ef058c5402e1381e32abce58
Pistrie <6153719-Pistrie@users.noreply.gitlab.com>
2021-06-10 11:42:56 +0200

    Created dao file for statistics

---
+++ Backend/app/dao/statisticsDao.js
@@ -0,0 +1,10 @@
+const database = require('./database')
+const logger = require('tracer').console()
+
+module.exports = {
+    // placeholder
+    // callback to controller
+    // controller returns data to frontend
+    // callback(err, undefined)
+    // callback(undefined, result)
+}
