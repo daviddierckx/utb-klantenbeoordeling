6bc8e6230f3985b3c79c59a8b57ecf978b6a7e36
Pistrie <6153719-Pistrie@users.noreply.gitlab.com>
2021-06-14 10:33:49 +0200

    Created routes file for statistics

---
+++ Backend/app/router/statisticsRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const logger = require("tracer").console();
+const statistics_controller = require('./controllers/statisticsController')
+
+// middleware logger
+router.use(function timeLog(req, res, next) {
+    logger.log(req.originalUrl, 'Time:', Date.now(), 'data:', JSON.stringify(req.body), 'query:', JSON.stringify(req.query), 'params:', JSON.stringify(req.params))
+    next();
+});
+
+router.get('/feedback/statistics/radio', statistics_controller.getCountRadioButtonAnswers)
+
+module.exports = router
