992dbc5ebd66af0a2e352999c1415c92e17d2c05
Pistrie <6153719-Pistrie@users.noreply.gitlab.com>
2021-06-15 11:56:54 +0200

    maybe good addition?

--- Backend/app/router/controllers/statisticsController.js
+++ Backend/app/router/controllers/statisticsController.js
@@ -57,11 +57,47 @@ module.exports = {
                     error: err,
                 });
             }
-            logger.log("Got the count!", JSON.stringify(results));
-            return res.status(200).send({
-                success: true,
-                data: results,
-            });
+            // logger.log("Got the count!", JSON.stringify(results));
+            // return res.status(200).send({
+            //     success: true,
+            //     data: results,
+
+            // });// Maak lijst van vragen en gemiddeldes
+            var list = document.createElement("div");
+            list.className = "opmerkingenLijst";
+
+            for (let i = 0; i < results.length; i++) {
+                //maak 'opmerking box' div
+                var box = document.createElement("div");
+                box.className = "opmerkingenBox";
+                //maak 'vraag ' p
+                var vraag = document.createElement("p");
+                vraag.className = "vraag";
+                vraag.innerText = results.name;
+                //maak 'gemiddelde' p
+                var positief = document.createElement("p");
+                positief.className = "gemiddelde";
+                positief.innerText = "Positief:", results.AantalPositief;
+                var neutraal = document.createElement("p");
+                neutraal.className = "gemiddelde";
+                neutraal.innerText = "Neutraal:", results.AantalNeutraal;
+                var negatief = document.createElement("p");
+                negatief.className = "gemiddelde";
+                negatief.innerText = "Negatief:", results.AantalNegatief;
+
+                // voeg vraag en gemiddelde toe aan box
+                box.appendChild(vraag);
+                box.appendChild(positief);
+                box.appendChild(neutraal);
+                box.appendChild(negatief);
+
+                // voeg toe aan lijst
+                list.appendChild(box);
+
+                // add to body
+                var element = document.getElementsByClassName("overzicht")[0];
+                element.appendChild(list);
+            }
         });
     },
 };
