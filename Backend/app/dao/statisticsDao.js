8ad16fb60768a0fa6a5c9be1025242ce95c3645f
Pistrie <6153719-Pistrie@users.noreply.gitlab.com>
2021-06-21 13:12:18 +0200

    Get CountOfRadioButtons from specific year

--- Backend/app/dao/statisticsDao.js
+++ Backend/app/dao/statisticsDao.js
@@ -33,7 +33,7 @@ module.exports = {
     });
   },
 
-  getAveragesFromSpecificYear(year, callback) {
+  getAverageRatingsFromSpecificYear(year, callback) {
     const query = {
       sql: `SELECT Question.questionTitle, AVG(Answer.answer)
             FROM Question
@@ -66,7 +66,6 @@ module.exports = {
   getCountOfRadioButtons(callback) {
     logger.log("Executing query");
     const query = {
-      // sql: "SELECT Q.questionTitle, (SELECT COUNT(A.answer) FROM Answer AS A WHERE A.answer = 'positief') AS AantalPositief, (SELECT COUNT(A.answer) FROM Answer AS A WHERE A.answer = 'neutraal') AS AantalNeutraal, (SELECT COUNT(A.answer) FROM Answer AS A WHERE A.answer = 'negatief') AS AantalNegatief FROM Question AS Q JOIN Answer AS A ON A.questionId = Q.id WHERE Q.questionType = 'radio' AND Q.questionTitle <> 'Product groep' GROUP BY Q.id;",
       sql: `SELECT Q.questionTitle
 
                   ,      sum(CASE
@@ -88,6 +87,54 @@ module.exports = {
                   WHERE  Q.questionType = 'radio'
                   AND    Q.questionTitle <> 'Product groep'
                   GROUP  BY Q.id;`,
+      timeout: 3000,
+    };
+    database.con.query(query, (err, results) => {
+      if (results.length == 0) {
+        logger.log("No data");
+        callback("No data", undefined);
+      } else if (err) {
+        logger.log("An error occured");
+        callback(err, undefined);
+      }
+      logger.log("OK. Returning results");
+      // logger.log(results)
+      callback(undefined, results);
+    });
+  },
+
+  getCountOfRadioButtonsFromSpecificYear(year, callback) {
+    logger.log("Executing query");
+    const query = {
+      sql: `SELECT Q.questionTitle
+
+            ,      sum(CASE
+                      when A.answer = 'positief' then 1
+                      else 0
+                   end)                                                      as AantalPositief
+            /*,     (SELECT COUNT(A.answer) FROM Answer AS A WHERE A.answer = 'positief' and A.questionId = Q.id ) AS AantalPositief_old  */
+            ,      sum(CASE
+                      when A.answer = 'neutraal' then 1
+                      else 0
+                  end)                                                      as AantalNeutraal
+            /* ,     (SELECT COUNT(A.answer) FROM Answer AS A WHERE A.answer = 'neutraal' and A.questionId = Q.id ) AS AantalNeutraal_old */
+            ,      sum(CASE
+                      when A.answer = 'negatief' then 1
+                      else 0
+                   end)                                                      as AantalNegatief
+            /*,     (SELECT COUNT(A.answer) FROM Answer AS A WHERE A.answer = 'negatief' and A.questionId = Q.id ) AS Old_AantalNegatief */
+            FROM   Question AS Q JOIN Answer AS A ON A.questionId = Q.id
+            WHERE A.entryId IN (
+           	SELECT   Answer.entryId
+            	FROM     Answer
+                JOIN   Question ON Answer.questionId = Question.id
+            	WHERE    Question.questionType = "date"
+            	AND      YEAR(Answer.answer) = ?
+            	GROUP BY Answer.entryId
+            )
+            AND    Q.questionType = 'radio'
+            AND    Q.questionTitle <> 'Product groep'
+            GROUP  BY Q.id;`,
       timeout: 3000,
     };
     database.con.query(query, (err, results) => {
