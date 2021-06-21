06ebaad253a8b3b04878317ecc0be75081b8fcc5
Pistrie <6153719-Pistrie@users.noreply.gitlab.com>
2021-06-21 14:43:57 +0200

    Added statistics routes

--- Backend/server.js
+++ Backend/server.js
@@ -6,12 +6,13 @@ const app = express();
 const routes = require("./app/router/routes");
 const logger = require("tracer").console();
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const serveStatic = require("serve-static");
 
 const routeAdmin = require("./app/router/admin");
 const routeLogin = require("./app/router/login");
 const routeBOF = require("./app/router/routeBOF");
+const routeStatistics = require("./app/router/statisticsRoutes");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -19,7 +20,7 @@ app.use(cookieParser());
 
 // middleware logger
 app.use(function timeLog(req, res, next) {
-  logger.log(req.originalUrl, 'Time:', Date.now(), 'data:', JSON.stringify(req.body), 'query:', JSON.stringify(req.query), 'params:', JSON.stringify(req.params))
+  logger.log(req.originalUrl, "Time:", Date.now(), "data:", JSON.stringify(req.body), "query:", JSON.stringify(req.query), "params:", JSON.stringify(req.params));
   next();
 });
 
@@ -35,9 +36,9 @@ app.use(express.static("public"));
 
 app.set("view engine", "hbs");
 
-app.get('/', (req, res) => { res.redirect("login") });
+app.get("/", (req, res) => { res.redirect("login"); });
 app.use("/api", routes);
-app.use("/admin", routeAdmin);
+app.use("/admin", routeAdmin, routeStatistics);
 app.use("/login", routeLogin);
 app.use("/beoordelingsformulier", routeBOF);
 app.use(serveStatic("./views"));
