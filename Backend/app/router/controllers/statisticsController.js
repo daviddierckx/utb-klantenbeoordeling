4dcdb27b7baf222caabf64bc763b67bbd0d12663
ArendKramer <72733811+ArendKramer@users.noreply.github.com>
2021-06-22 10:21:03 +0200

    added changes

--- Backend/app/router/controllers/statisticsController.js
+++ Backend/app/router/controllers/statisticsController.js
@@ -3,8 +3,8 @@ const statistics_dao = require("../../dao/statisticsDao");
 
 module.exports = {
   allData: (req, res) => {
-    statistics_dao.getAveragesFromRating((err, results)) {
+    statistics_dao.getAveragesFromRating((err, results)); {
       console.log("Hello");
-    };
+    }
   }
 };
